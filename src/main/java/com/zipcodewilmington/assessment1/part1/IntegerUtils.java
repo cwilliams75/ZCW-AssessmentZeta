bd1111582160c583d2026ee25fdd742a0e6531f4
cwilliams <coreywllms9@gmail.com>
2020-02-17 14:55:30 -0500

    Completed BasicArray, BasicString,InterArray,1 on IntegerUtils

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -11,6 +11,7 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
+
         return null;
     }
 
@@ -27,6 +28,13 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+        int num = 12345, reversed = 0;
+        while (num != 0){
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        return reversed;
     }
 }
